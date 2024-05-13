d83f684fc4a06b3077a5ef070f1437181c1ff36f
Mehmet Eren Öztürk <222803065@ogr.cbu.edu.tr>
2024-05-13 23:26:26 +0300

    seconderen

--- YazilimYapimi/Form6.cs
+++ YazilimYapimi/Form6.cs
@@ -59,7 +59,7 @@ namespace YazilimYapimi
                     }
                     else
                     {
-                        MessageBox.Show("No word updated");
+                        MessageBox.Show("No word");
                     }
                 }
             }
